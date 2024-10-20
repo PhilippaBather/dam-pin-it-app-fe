e58b50975f6379377fe08695dd2c99ee43cdf46a
pabather <p.a.bather1@gmail.com>
2024-10-21 00:26:44 +0200

    added placeholder pages and private routing

---
+++ src/pages/routes/routes.tsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react-refresh/only-export-components
+export default Object.freeze({
+  HOME: "/",
+  ERROR: "/error",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  PASSWORD_RECOVERY: "/password-recovery",
+  PROJECT_HOME_PAGE: "/projects-home",
+});
