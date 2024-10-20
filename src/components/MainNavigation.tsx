be2ab068a141e5df4d47aad174933bb1aea2419d
pabather <p.a.bather1@gmail.com>
2024-10-21 00:26:12 +0200

    added placeholder for main navigationi

---
+++ src/components/MainNavigation.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+// NavLink does not send new HTTP request and uses an anonymous function to indicate whether a lin is active
+// add classes when doing SASS style sheets
+// end indicates that the path ends with a slash, otherwise all urls beginning with a forward slash would be indicated as being active
+// <NavLink to="/" className={(isActive) => (isActive ? classes.active : undefined) end} >Home</NavLink>
+
+// useNavigate hook: allows you to switch to a different route programmatically inside your code
+
+import ROUTES from "../pages/routes/routes";
+
+function MainNavigation() {
+  return (
+    <>
+      <ul>
+        <li>
+          <NavLink to={ROUTES.HOME}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={ROUTES.LOGIN}>Login</NavLink>
+        </li>
+        <li>
+          <NavLink to={ROUTES.REGISTER}>Register</NavLink>
+        </li>
+        <li>
+          <NavLink to={ROUTES.PROJECT_HOME_PAGE}>Account</NavLink>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default MainNavigation;
